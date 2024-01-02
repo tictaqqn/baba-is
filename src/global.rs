a68a6c3b8a077d2f6e91eedce2ba9b3f0366ba9a
bababax11 <38175513+bababax11@users.noreply.github.com>
2024-01-02 13:09:33 +0900

    test: add tests

--- src/global.rs
+++ src/global.rs
@@ -94,7 +94,82 @@ impl Direction {
 
 #[cfg(test)]
 mod tests {
-    use crate::global::Direction;
+    use crate::global::*;
+
+    #[test]
+    fn test_is_subject() {
+        use Entity::*;
+        assert!(BabaB.is_subject());
+        assert!(FlagB.is_subject());
+        assert!(WallB.is_subject());
+        assert!(RockB.is_subject());
+        assert!(WaterB.is_subject());
+        assert!(LavaB.is_subject());
+        assert!(SkullB.is_subject());
+        assert!(!Baba.is_subject());
+        assert!(!Flag.is_subject());
+        assert!(!Wall.is_subject());
+        assert!(!Rock.is_subject());
+        assert!(!Water.is_subject());
+        assert!(!Lava.is_subject());
+        assert!(!Skull.is_subject());
+        assert!(!Is.is_subject());
+        assert!(!You.is_subject());
+        assert!(!Push.is_subject());
+        assert!(!Stop.is_subject());
+        assert!(!Win.is_subject());
+        assert!(!Defeat.is_subject());
+    }
+
+    #[test]
+    fn test_is_verb() {
+        use Entity::*;
+        assert!(Is.is_verb());
+        assert!(!You.is_verb());
+        assert!(!Push.is_verb());
+        assert!(!Stop.is_verb());
+        assert!(!Win.is_verb());
+        assert!(!Defeat.is_verb());
+        assert!(!BabaB.is_verb());
+        assert!(!FlagB.is_verb());
+        assert!(!WallB.is_verb());
+        assert!(!RockB.is_verb());
+        assert!(!WaterB.is_verb());
+        assert!(!LavaB.is_verb());
+        assert!(!SkullB.is_verb());
+        assert!(!Baba.is_verb());
+        assert!(!Flag.is_verb());
+        assert!(!Wall.is_verb());
+        assert!(!Rock.is_verb());
+        assert!(!Water.is_verb());
+        assert!(!Lava.is_verb());
+        assert!(!Skull.is_verb());
+    }
+
+    #[test]
+    fn test_is_object() {
+        use Entity::*;
+        assert!(!BabaB.is_object());
+        assert!(!FlagB.is_object());
+        assert!(!WallB.is_object());
+        assert!(!RockB.is_object());
+        assert!(!WaterB.is_object());
+        assert!(!LavaB.is_object());
+        assert!(!SkullB.is_object());
+        assert!(!Baba.is_object());
+        assert!(!Flag.is_object());
+        assert!(!Wall.is_object());
+        assert!(!Rock.is_object());
+        assert!(!Water.is_object());
+        assert!(!Lava.is_object());
+        assert!(!Skull.is_object());
+        assert!(!Is.is_object());
+        assert!(You.is_object());
+        assert!(Push.is_object());
+        assert!(Stop.is_object());
+        assert!(Win.is_object());
+        assert!(Defeat.is_object());
+    }
 
     #[test]
     fn test_direction() {
